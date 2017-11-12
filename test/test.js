baefe708e45af2e89b2734d7deeb187339dbc610
Christian Muehlhaus <christian@noctaru.de>
2017-11-12 19:38:36 +0100

    test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+describe('Test', function() {
+    describe('first', function() {
+        it('should run without error', function() {
+           console.log("Hallo");
+        });
+    });
+    describe('second()', function() {
+        it('should run with error', function() {
+            console.log("Hallo");
+         //   throw new Error("Buh!");
+        });
+    });
+});
+
